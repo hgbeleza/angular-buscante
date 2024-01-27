fcd080d6b1a1ca7e2b5162c10cd66fc203e923d7
hgbeleza <developer.beleza06@gmail.com>
2024-01-26 23:19:06 -0300

    aprendendo sobre o Rxjs

---
+++ src/app/service/livro.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map, tap } from 'rxjs';
+import { Item, LivrosResultado } from '../models/interfaces';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LivroService {
+  // https://www.googleapis.com/books/v1/volumes?q=search+terms
+  private readonly api = 'https://www.googleapis.com/books/v1/volumes';
+
+  constructor(private http: HttpClient) {}
+
+  buscar(valorDigitado: string): Observable<Item[]> {
+    const params = new HttpParams().append('q', valorDigitado);
+    return this.http.get<LivrosResultado>(this.api, { params }).pipe(
+      tap((retornoAPI) => console.log("Retorno 1", retornoAPI)),
+      map((resultado) => resultado.items),
+      tap(resultado => console.log("Retorno 2", resultado)
+      )
+    );
+  }
+}
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy {
 
   listaLivros: [];
+  campoBusca: string = '';
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private livroService: LivroService) { }
 
+  buscarLivros() {
+    this.subscription = this.livroService.buscar(this.campoBusca).subscribe({
+      next: retornoAPI => console.log(),
+      error: err => console.log(err),
+      complete: () => console.log('Observable completado')
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
 
 
